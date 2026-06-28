09c5a4c69142392dd1929fe8bab72faa342bdef7
mubarakhassen737 <mubarakhassen737@gmail.com>
2026-06-28 06:50:15 +0300

    Implement majorityElement method in Solution class

--- majority element.py
+++ majority element.py
@@ -1,8 +1,4 @@
 class Solution:
-    def plusOne(self, q):
-        for w in range(len(q) - 1, -1, -1):
-            if q[w] < 9:
-                q[w] += 1
-                return q
-            q[w] = 0
-        return [1] + q
+    def majorityElement(self, a):
+        a.sort()
+        return a[len(a) // 2]
